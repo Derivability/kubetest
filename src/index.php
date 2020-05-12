b08bcaf66565a3ff0eaddad334de26916e716df1
Bogdan Shchogolev <bogdan993000@gmail.com>
2020-05-12 22:27:23 +0300

    Add k8s services configs
    Added configs for k8s services.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?
-$dbconn = pg_connect("host=pg_db dbname=my_db user=db_user password=password");
+$dbconn = pg_connect("host=postgresql-service dbname=my_db user=db_user password=password");
 $result = pg_query($dbconn, "select * from tabla");
 while ($row = pg_fetch_row($result)) {
   echo "$row[0] $row[1] $row[2]\n";
